ca1cef214f8a283f75e9c8be2c240fb76243c16d
Cruz Sanchez <csanchez@truecoders.io>
2020-01-27 12:55:59 -0600

    added gitignore

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -6,7 +6,7 @@ namespace ChallengesWithTestsMark8
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
-            throw new NotImplementedException();
+            throw new NotImplementedException();            
         }
 
         public double Subtract(double minuend, double subtrahend)
